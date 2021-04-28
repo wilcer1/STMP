ea2d6ff99f055ef23fbb599fa036ec0243509a0a
TimmyBergvall <timmy.bergvall@hotmail.com>
2021-04-28 15:34:39 +0200

    New features in menu

--- menu.py
+++ menu.py
@@ -5,22 +5,6 @@ def main():
     stmpMenu()
 
 
-def stmpMenu():
-    userInput = input("\nWelcome"
-    "\n1. Make a budget"
-    "\n2. Create long-term savings"
-    "\n3. Sew how much you can save \n")
-
-    if userInput == "1":
-        budget()
-
-    elif userInput == "2":
-        saving()
-
-    elif userInput == "3":
-        calculateSave()
-
-
 def signIn():
     userInput = input("Welcome to STMP" 
     "\n1. Sign in "
@@ -36,16 +20,43 @@ def signIn():
         username = input("Enter a username: ")
         password = input("Enter a password: ")
         password1 = input("Confirm your password: ")
+        income = input("Enter your monthly income: ")
+        expenses = input("Enter your monthly expenses: ")
 
     else:
         print("That is not valid, enter 1 or 2 \n")
         signIn()
 
+
+def stmpMenu():
+    userInput = input("\nWelcome"
+    "\n1. Make a budget"
+    "\n2. Create long-term savings"
+    "\n3. Sew how much you can save \n")
+
+    if userInput == "1":
+        budget()
+
+    elif userInput == "2":
+        saving()
+
+    elif userInput == "3":
+        calculateSave()
+
+    else:
+        print("That is not valid, enter 1 or 2 \n")
+        stmpMenu()
+
+
 def budget():
     pass
 
+
 def saving():
-    pass
+    savingAmount = int(input("How much do you want to save? (In SEK) "))
+    savingTime = int(input("Enter when you want to accomplish the saving (Enter in month): "))
+    moneyPerMonth = savingAmount / savingTime
+    print("To accomplish your goal, you need to save", moneyPerMonth, "SEK in", savingTime, "month!")
 
 def calculateSave():
     pass
