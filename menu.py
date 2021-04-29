3959b07df3ef48d53ad9cb046455362190dbdf0a
TimmyBergvall <timmy.bergvall@hotmail.com>
2021-04-29 11:42:38 +0200

    Update on menu

--- menu.py
+++ menu.py
@@ -2,7 +2,6 @@
 
 def main():
     signIn()
-    stmpMenu()
 
 
 def signIn():
@@ -11,34 +10,50 @@ def signIn():
     "\n2. Register \n")
 
     if userInput == "1":
-        username = input("Enter your username: ")
+        email = input("Enter your email: ")
         password = input("Enter your password: ")
+
+
+        # QUERY: "SELECT Account.Email, Account.Password FROM Account WHERE Account.Email =", email, ";"
+
+
+        stmpMenu()
 
     elif userInput == "2":
         firstName = input("Enter your firstname: ")
         lastName = input("Enter your lastname: ")
-        username = input("Enter a username: ")
         password = input("Enter a password: ")
         password1 = input("Confirm your password: ")
+        email = input("Enter your email: ")
         income = input("Enter your monthly income: ")
         expenses = input("Enter your monthly expenses: ")
 
+        if password == password1:
+            print("Test")
+            # QUERY: "INSERT INTO Account VALUES("firstName", "lastName", "password", "email", "income", "expenses");"
+
+        else:
+            print("\n \nPassword did not match, try again")
+            signIn()
+
+        stmpMenu()
+
     else:
-        print("That is not valid, enter 1 or 2 \n")
+        print("\nThat is not valid, enter 1 or 2")
         signIn()
 
 
 def stmpMenu():
     userInput = input("\nWelcome"
-    "\n1. Make a budget"
-    "\n2. Create long-term savings"
-    "\n3. Sew how much you can save \n")
+    "\n1. Make a budget" # budget()
+    "\n2. Create long-term savings" # crateSaving()
+    "\n3. Sew how much you can save \n") # calculateSave()
 
     if userInput == "1":
         budget()
 
     elif userInput == "2":
-        saving()
+        createSaving()
 
     elif userInput == "3":
         calculateSave()
@@ -52,7 +67,7 @@ def budget():
     pass
 
 
-def saving():
+def createSaving():
     savingAmount = int(input("How much do you want to save? (In SEK) "))
     savingTime = int(input("Enter when you want to accomplish the saving (Enter in month): "))
     moneyPerMonth = savingAmount / savingTime
