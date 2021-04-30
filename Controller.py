eedbacc16d151b4e9b89acb7c93e493c6732350b
wilcer1 <wille.cervin@live.se>
2021-04-30 09:52:52 +0200

    Added budgetscreen

--- Controller.py
+++ Controller.py
@@ -202,6 +202,153 @@ class Ui_BudgetChoiceScreen(object):
         self.pushButton_3.setText(_translate("MainWindow", "Make your own budget"))
         self.pushButton.setText(_translate("MainWindow", "Back"))
 
+class Ui_BudgetScreen(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(320, 40, 121, 21))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(330, 80, 118, 23))
+        self.progressBar.setProperty("value", 100)
+        self.progressBar.setObjectName("progressBar")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(360, 60, 47, 13))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(140, 150, 47, 13))
+        self.label_3.setObjectName("label_3")
+        self.progressBar_2 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_2.setGeometry(QtCore.QRect(110, 190, 118, 23))
+        self.progressBar_2.setProperty("value", 20)
+        self.progressBar_2.setObjectName("progressBar_2")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(140, 170, 47, 13))
+        self.label_4.setObjectName("label_4")
+        self.progressBar_3 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_3.setGeometry(QtCore.QRect(110, 290, 118, 23))
+        self.progressBar_3.setProperty("value", 20)
+        self.progressBar_3.setObjectName("progressBar_3")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(110, 250, 131, 16))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(140, 270, 47, 13))
+        self.label_6.setObjectName("label_6")
+        self.progressBar_4 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_4.setGeometry(QtCore.QRect(110, 390, 118, 23))
+        self.progressBar_4.setProperty("value", 20)
+        self.progressBar_4.setObjectName("progressBar_4")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(130, 350, 47, 13))
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(140, 370, 47, 13))
+        self.label_8.setObjectName("label_8")
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(350, 270, 47, 13))
+        self.label_9.setObjectName("label_9")
+        self.progressBar_5 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_5.setGeometry(QtCore.QRect(320, 290, 118, 23))
+        self.progressBar_5.setProperty("value", 20)
+        self.progressBar_5.setObjectName("progressBar_5")
+        self.progressBar_6 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_6.setGeometry(QtCore.QRect(320, 390, 118, 23))
+        self.progressBar_6.setProperty("value", 20)
+        self.progressBar_6.setObjectName("progressBar_6")
+        self.label_10 = QtWidgets.QLabel(self.centralwidget)
+        self.label_10.setGeometry(QtCore.QRect(340, 350, 47, 13))
+        self.label_10.setObjectName("label_10")
+        self.progressBar_7 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_7.setGeometry(QtCore.QRect(320, 190, 118, 23))
+        self.progressBar_7.setProperty("value", 20)
+        self.progressBar_7.setObjectName("progressBar_7")
+        self.label_11 = QtWidgets.QLabel(self.centralwidget)
+        self.label_11.setGeometry(QtCore.QRect(350, 150, 47, 13))
+        self.label_11.setObjectName("label_11")
+        self.label_12 = QtWidgets.QLabel(self.centralwidget)
+        self.label_12.setGeometry(QtCore.QRect(320, 250, 131, 16))
+        self.label_12.setObjectName("label_12")
+        self.label_13 = QtWidgets.QLabel(self.centralwidget)
+        self.label_13.setGeometry(QtCore.QRect(350, 170, 47, 13))
+        self.label_13.setObjectName("label_13")
+        self.label_14 = QtWidgets.QLabel(self.centralwidget)
+        self.label_14.setGeometry(QtCore.QRect(350, 370, 47, 13))
+        self.label_14.setObjectName("label_14")
+        self.label_15 = QtWidgets.QLabel(self.centralwidget)
+        self.label_15.setGeometry(QtCore.QRect(570, 370, 47, 13))
+        self.label_15.setObjectName("label_15")
+        self.label_16 = QtWidgets.QLabel(self.centralwidget)
+        self.label_16.setGeometry(QtCore.QRect(570, 270, 47, 13))
+        self.label_16.setObjectName("label_16")
+        self.label_17 = QtWidgets.QLabel(self.centralwidget)
+        self.label_17.setGeometry(QtCore.QRect(540, 250, 131, 16))
+        self.label_17.setObjectName("label_17")
+        self.progressBar_8 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_8.setGeometry(QtCore.QRect(540, 190, 118, 23))
+        self.progressBar_8.setProperty("value", 20)
+        self.progressBar_8.setObjectName("progressBar_8")
+        self.progressBar_9 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_9.setGeometry(QtCore.QRect(540, 290, 118, 23))
+        self.progressBar_9.setProperty("value", 20)
+        self.progressBar_9.setObjectName("progressBar_9")
+        self.label_18 = QtWidgets.QLabel(self.centralwidget)
+        self.label_18.setGeometry(QtCore.QRect(560, 350, 47, 13))
+        self.label_18.setObjectName("label_18")
+        self.progressBar_10 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_10.setGeometry(QtCore.QRect(540, 390, 118, 23))
+        self.progressBar_10.setProperty("value", 20)
+        self.progressBar_10.setObjectName("progressBar_10")
+        self.label_19 = QtWidgets.QLabel(self.centralwidget)
+        self.label_19.setGeometry(QtCore.QRect(570, 150, 47, 13))
+        self.label_19.setObjectName("label_19")
+        self.label_20 = QtWidgets.QLabel(self.centralwidget)
+        self.label_20.setGeometry(QtCore.QRect(570, 170, 47, 13))
+        self.label_20.setObjectName("label_20")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Total Monthly Budget:"))
+        self.label_2.setText(_translate("MainWindow", "SEK"))
+        self.label_3.setText(_translate("MainWindow", "Food"))
+        self.label_4.setText(_translate("MainWindow", "SEK"))
+        self.label_5.setText(_translate("MainWindow", "Fixed Expenses"))
+        self.label_6.setText(_translate("MainWindow", "SEK"))
+        self.label_7.setText(_translate("MainWindow", "TextLabel"))
+        self.label_8.setText(_translate("MainWindow", "SEK"))
+        self.label_9.setText(_translate("MainWindow", "SEK"))
+        self.label_10.setText(_translate("MainWindow", "TextLabel"))
+        self.label_11.setText(_translate("MainWindow", "Food"))
+        self.label_12.setText(_translate("MainWindow", "Fixed Expenses"))
+        self.label_13.setText(_translate("MainWindow", "SEK"))
+        self.label_14.setText(_translate("MainWindow", "SEK"))
+        self.label_15.setText(_translate("MainWindow", "SEK"))
+        self.label_16.setText(_translate("MainWindow", "SEK"))
+        self.label_17.setText(_translate("MainWindow", "Fixed Expenses"))
+        self.label_18.setText(_translate("MainWindow", "TextLabel"))
+        self.label_19.setText(_translate("MainWindow", "Food"))
+        self.label_20.setText(_translate("MainWindow", "SEK"))
+    
+
 
 
 class LoginScreen(QMainWindow, Ui_LoginScreen):
@@ -279,31 +426,29 @@ class BudgetChoiceScreen(QMainWindow, Ui_BudgetChoiceScreen):
         self.setupUi(self) # Run the code that creates the UI layout
         self.pushButton_3.clicked.connect(self.Manual) # Call function when button is pressed
         self.pushButton_2.clicked.connect(self.Auto) #  -""-
-        self.pushButton.clicked.connect(self.Back) #    -""-
+        self.pushButton.clicked.connect(self.goBack) #    -""-
 
     def Manual(self):
-        pass
+        self.displayUi = BudgetScreen() 
+        self.hide()
+        self.displayUi.show()
     def Auto(self):
         pass
-    def Back(self):
-        self.displayUi = MenuScreen() 
+    def goBack(self):
+        self.displayUi = BudgetScreen() 
         self.hide()
         self.displayUi.show()
 
+class BudgetScreen(QMainWindow, Ui_BudgetScreen):
+    # Budget Screen Controller
+    def __init__(self):
+        super().__init__() # Call the superclass constructor
+        self.setupUi(self) # Run the code that creates the UI layout
 
 
 if __name__ == "__main__":
     import sys
-    # app = QtWidgets.QApplication(sys.argv)
-    # MainWindow = QtWidgets.QMainWindow()
-    # MainScreen = Ui_MainWindow()
-    # MainScreen.setupUi(MainWindow)
-   
-
-    # MainWindow.show()
-    # sys.exit(app.exec_())
-
     app = QApplication(sys.argv)
-    MainWindow = LoginScreen() 
+    MainWindow = LoginScreen() # Use the login screen as the mainwindow to start
     MainWindow.show()
     sys.exit(app.exec_())
