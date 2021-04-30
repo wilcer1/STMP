0f3c11963c46498e0e565b113e80e86ed31deada
Luehunk <lucas.carlsson@outlook.com>
2021-04-30 12:33:10 +0200

    Create queries for getting information from database

--- database_connection.py
+++ database_connection.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 
+connection = None
 
 try:
     connection = mysql.connector.connect(
@@ -19,7 +20,7 @@ except Error as e:
 
 def disconnect():
     try:
-        if connection.is_connected():
+        if connection is not None and connection.is_connected():
             mycursor.close()
             connection.close()
             print("Disconnected")
@@ -37,3 +38,47 @@ def showTables():
 
     for x in myresult:
         print(x)
+
+
+def get_first_name(email):
+    """Gets first name from database"""
+
+    mycursor.execute("SELECT first_name FROM account WHERE email ='" + email + "';")
+    myresult = mycursor.fetchone()
+
+    return myresult
+
+
+def get_last_name(email):
+
+    mycursor.execute("SELECT last_name FROM account WHERE email ='" + email + "';")
+    myresult = mycursor.fetchone()
+
+    return myresult
+
+
+def get_income(email):
+
+    mycursor.execute("SELECT income FROM account WHERE email ='" + email + "';")
+    myresult = mycursor.fetchone()
+
+    return myresult
+
+
+def get_expenses(email):
+
+    mycursor.execute("SELECT expenses FROM account WHERE email ='" + email + "';")
+    myresult = mycursor.fetchone()
+
+    return myresult
+
+
+def verify_login(email, password):
+
+    mycursor.execute("SELECT password FROM account WHERE email ='" + email + "';")
+    myresult = mycursor.fetchone()
+
+    if password == myresult:
+        return True
+    else:
+        return False
