0173eff43cfedfd8f41a788a220c71f579515fae
Luehunk <lucas.carlsson@outlook.com>
2021-05-06 15:04:17 +0200

    fixed database_connection_test

--- database_queries.py
+++ database_queries.py
@@ -5,8 +5,6 @@ from account import Account
 
 customer = Account()
 customer = customer.getCustomer()
-
-database_connection.show_account()
 print(database_connection.get_first_name("lucas@gmail.com"))
 print(database_connection.verify_login("lucas@gmail.com", "Lucas123"))
 database_connection.disconnect()
