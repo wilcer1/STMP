ae697159c3bbd4ab667d675f6a46477b8af25a88
wilcer1 <wille.cervin@live.se>
2021-05-10 14:02:50 +0200

    inted all inputs in firstloginscren

--- budget.py
+++ budget.py
@@ -10,3 +10,15 @@ class Budget():
        self.income = income
        self.variable_expenses = variable_expenses
        self.fixed_expenses = fixed_expenses
+
+    def get_total_expenses(self):
+        """Add the expenses together and return."""
+        for key in self.variable_expenses:
+            total_var = 0
+            total_var += self.variable_expenses[key]
+
+        for key in self.fixed_expenses:
+            total_fix = 0
+            total_fix += self.fixed_expenses[key]
+
+        return total_fix, total_var
