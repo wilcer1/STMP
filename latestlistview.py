f517b86fd273d3bfb734653cebffac17a45d5215
TimmyBergvall <timmy.bergvall@hotmail.com>
2021-05-11 08:57:33 +0200

    Logo is on everything except savinggoal.ui

--- latestlistview.py
+++ latestlistview.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 572)
+        MainWindow.resize(800, 650)
         MainWindow.setStyleSheet("background-color: rgb(3, 130, 168);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
